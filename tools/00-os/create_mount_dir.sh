2b0fc47aa07ed682cdfe2314fcbc9b388313002d
suxiaohun <h13616489001@gmail.com>
2023-08-30 11:23:49 +0800

    Fix mount shell not exit when sub command fail

--- tools/00-os/create_mount_dir.sh
+++ tools/00-os/create_mount_dir.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+set -e
+
 function create_mount_dir() {
-    name="$1"
-    disks_dir="/sususu/disks"
-    locals_dir="/sususu/locals"
+  name="$1"
+  volume_param="$2"
+  disks_dir="/sususu/disks"
+  locals_dir="/sususu/locals"
 
-    # create dir
-    mkdir -p "$disks_dir/$name/volume0"
-    mkdir -p "$locals_dir/$name/volume0"
-
-    # mount
-    sudo mount --bind "$disks_dir/$name/volume0" "$locals_dir/$name/volume0"
-
-    # persist
-    echo "$disks_dir/$name/volume0 $locals_dir/$name/volume0 none bind 0 0" | sudo tee -a /etc/fstab
+  for i in $(seq 0 "${volume_param:-0}"); do
+    volume="volume$i"
+    disks_volume_dir="$disks_dir/$name/$volume"
+    if [ ! -d "$disks_volume_dir" ]; then
+      mkdir -p "$disks_dir/$name/$volume"
+      mkdir -p "$locals_dir/$name/$volume"
+      sudo mount --bind "$disks_dir/$name/$volume" "$locals_dir/$name/$volume"
+      echo "$disks_dir/$name/$volume $locals_dir/$name/$volume none bind 0 0" | sudo tee -a /etc/fstab
+    fi
+  done
 }
 
 # check param
 if [ -z "$1" ]; then
-    echo "You must pass an dir name"
+  echo "You must pass at least a dir name"
 else
-    create_mount_dir "$1"
+  create_mount_dir "$1" "$2"
 fi
